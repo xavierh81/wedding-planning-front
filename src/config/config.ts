81283b9e3a87bd7792d8628ddab9e6fd0f29946a
Xavier HEYSCH <xavier@codefever.fr>
2022-09-02 10:32:41 +0200

    Add configuration for production env

--- src/config/config.ts
+++ src/config/config.ts
@@ -14,7 +14,17 @@ const config = {
         }
     },
     production: {
-        googleMapsApiKey: process.env.REACT_APP_WEDDING_PLANNING_GOOGLE_MAPS_API_KEY
+        googleMapsApiKey: process.env.REACT_APP_WEDDING_PLANNING_GOOGLE_MAPS_API_KEY,
+        weddingSettings: {
+            date: "2023-07-01",
+            place: "Château de Mauriac"
+        },
+        api: {
+            url: "https://dg1k71yitl.execute-api.eu-west-3.amazonaws.com/Development",
+            endpoints: {
+                rsvpRequest: "/rsvp_request"
+            }
+        }
     }
 }
 
