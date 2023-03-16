ba82b698faca5e8e3fd48e3879aeac01c2f3aa2e
Xavier HEYSCH <xavier@codefever.fr>
2023-03-16 21:37:53 +0100

    Update homepage with planning

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -35,11 +35,47 @@ function Home() {
 
             <Row>
                 <Col {...DEFAULT_SMALL_GRID}>
-                    <div className="homeBloc">
+                    <div className="homeBloc" style={{display:"none"}}>
                         <span className="title">{t`homepage_rsvp_title`}</span>
                         <span className="desc">{t`homepage_rsvp_desc`}</span>
 
                         <RoundedButton text={t`homepage_rsvp_button`} onClick={() => navigate(SiteRoutes.RSVP_FORM)} />
+                    </div>
+
+                    <div className="homeBloc">
+                        <span className="title">{t`homepage_planning_title`}</span>
+
+                        <div id="planning">
+                            <div className="planningItem">
+                                <span className="hour">{t`homepage_planning_item_1_hour`}</span>
+                                <span className="desc">{t`homepage_planning_item_1_desc`}</span>
+                                <span className="additionalDesc">{t`homepage_planning_item_1_additional_desc`}</span>
+                            </div>
+                            <div className="planningItem">
+                                <span className="hour">{t`homepage_planning_item_2_hour`}</span>
+                                <span className="desc">{t`homepage_planning_item_2_desc`}</span>
+                            </div>
+                            <div className="planningItem">
+                                <span className="hour">{t`homepage_planning_item_3_hour`}</span>
+                                <span className="desc">{t`homepage_planning_item_3_desc`}</span>
+                            </div>
+                            <div className="planningItem">
+                                <span className="hour">{t`homepage_planning_item_4_hour`}</span>
+                                <span className="desc">{t`homepage_planning_item_4_desc`}</span>
+                            </div>
+                            <div className="planningItem">
+                                <span className="hour">{t`homepage_planning_item_5_hour`}</span>
+                                <span className="desc">{t`homepage_planning_item_5_desc`}</span>
+                            </div>
+                            <div className="planningItem">
+                                <span className="hour">{t`homepage_planning_item_6_hour`}</span>
+                                <span className="desc">{t`homepage_planning_item_6_desc`}</span>
+                            </div>
+                            <div className="planningItem">
+                                <span className="hour">{t`homepage_planning_item_7_hour`}</span>
+                                <span className="desc">{t`homepage_planning_item_7_desc`}</span>
+                            </div>
+                        </div>
                     </div>
                 </Col>
             </Row>
